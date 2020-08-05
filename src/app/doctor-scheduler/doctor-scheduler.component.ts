529086a513c4591265ed06c49f92b8362b6413f5
dawid <dawids8788@gmail.com>
2020-08-05 18:25:17 +0200

    Doctor-scheduler component created

---
+++ src/app/doctor-scheduler/doctor-scheduler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-scheduler',
+  templateUrl: './doctor-scheduler.component.html',
+  styleUrls: ['./doctor-scheduler.component.css']
+})
+export class DoctorSchedulerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
