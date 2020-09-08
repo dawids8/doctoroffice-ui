b606ae453c100de506f8cb71022b623d5f8f05f1
dawid <dawids8788@gmail.com>
2020-09-08 19:48:36 +0200

    New fields and methods in doctor-scheduler.component

--- src/app/doctor-scheduler/doctor-scheduler.component.ts
+++ src/app/doctor-scheduler/doctor-scheduler.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ScheduleService} from "../_services/schedule.service.service";
+import {Schedule, User} from "../_models";
+import {AuthenticationService} from "../_services";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-doctor-scheduler',
@@ -7,11 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorSchedulerComponent implements OnInit {
 
+  schedules: Schedule[];
+  currentUser: User;
+  currentUserSubscription: Subscription;
 
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private scheduleService: ScheduleService, private authenticationService: AuthenticationService) {
+    this.authenticationService.currentUser.subscribe(user => {
+      this.currentUser = user;
+    });
   }
 
+  ngOnInit(): void {
+    this.getSchedulesByDoctorId(this.currentUser.username);
+  }
+
+  getSchedulesByDoctorId(username: string): void {
+    this.scheduleService.getSchedulesByDoctorId(username).subscribe(response => {
+      this.schedules = response;
+      console.log(this.schedules);
+    });
+  }
 }
