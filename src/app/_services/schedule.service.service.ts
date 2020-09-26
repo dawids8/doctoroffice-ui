cb4797a79d4faad640902286012d297f79911e77
dawid <dawids8788@gmail.com>
2020-09-26 16:50:46 +0200

    New method added to schedule service

--- src/app/_services/schedule.service.service.ts
+++ src/app/_services/schedule.service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Schedule} from "../_models";
+import {UpdateScheduleRequest} from "../_models/update-schedule-request";
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,12 @@ export class ScheduleService {
 
   constructor(private http: HttpClient) { }
 
-  public getSchedulesByDoctorId(username: string): Observable<Schedule[]> {
+  public getSchedulesByDoctorUsername(username: string): Observable<Schedule[]> {
     const param = new HttpParams().set('username', username);
     return this.http.get<Schedule[]>('http://localhost:8081/schedule/getSchedule', {params: param});
+  }
+
+  public updateSchedules(updateScheduleRequest: UpdateScheduleRequest): Observable<Schedule[]> {
+     return this.http.post<Schedule[]>('http://localhost:8081/schedule/update', updateScheduleRequest);
   }
 }
