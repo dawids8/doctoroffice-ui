ee676269346e31ba63f57b6a4d8df4f5009e87ce
dawid <dawids8788@gmail.com>
2020-10-10 18:43:33 +0200

    Date picker with range added and configured with appointments.service.ts

--- src/app/_services/appointments.service.ts
+++ src/app/_services/appointments.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Appointment} from "../_models";
+import {Appointment, GenerateAppointmentsRequest, Schedule} from "../_models";
+import {UpdateScheduleRequest} from "../_models/update-schedule-request";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class AppointmentsService {
   public getAllAppointmentsByDoctorUsername(username: string): Observable<Appointment[]> {
     const param = new HttpParams().set('username', username);
     return this.http.get<Appointment[]>('http://localhost:8081/appointment/getAllByDoctorUsername', {params: param});
+  }
+
+  public generateAppointments(generateAppointmentsRequest: GenerateAppointmentsRequest) {
+    return this.http.post('http://localhost:8081/appointment/generateAppointments', generateAppointmentsRequest);
   }
 }
--- src/app/doctor-scheduler/doctor-scheduler.component.ts
+++ src/app/doctor-scheduler/doctor-scheduler.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnChanges, OnInit, ViewChild} from '@angular/core';
 import {ScheduleService} from "../_services/schedule.service.service";
-import {Schedule, ScheduleDay, User} from "../_models";
+import {GenerateAppointmentsRequest, Schedule, ScheduleDay, User} from "../_models";
 import {AuthenticationService} from "../_services";
 import {Subscription} from "rxjs";
 import {UpdateScheduleRequest} from "../_models/update-schedule-request";
+import {DateRange} from "@angular/material/datepicker";
+import {AppointmentsService} from "../_services/appointments.service";
 
 @Component({
   selector: 'app-doctor-scheduler',
@@ -15,21 +17,25 @@ export class DoctorSchedulerComponent implements OnInit {
   schedules: Schedule[];
   currentUser: User;
   currentUserSubscription: Subscription;
+  generateStart: Date;
+  generateEnd: Date;
+  currentDate: Date;
 
-  constructor(private scheduleService: ScheduleService, private authenticationService: AuthenticationService) {
+  constructor(private scheduleService: ScheduleService, private appointmentService: AppointmentsService, private authenticationService: AuthenticationService) {
     this.authenticationService.currentUser.subscribe(user => {
       this.currentUser = user;
     });
   }
 
   ngOnInit(): void {
+    this.currentDate = new Date();
     this.getSchedulesByDoctorId(this.currentUser.username);
   }
 
   getSchedulesByDoctorId(username: string): void {
     this.scheduleService.getSchedulesByDoctorUsername(username).subscribe(response => {
       this.schedules = response;
-      console.log(this.schedules);
+      // console.log(this.schedules);
     });
   }
 
@@ -37,11 +43,12 @@ export class DoctorSchedulerComponent implements OnInit {
     const updateScheduleRequest = new UpdateScheduleRequest(this.currentUser.username, this.schedules);
     this.scheduleService.updateSchedules(updateScheduleRequest).subscribe(response => {
       this.schedules = response;
-      console.log(this.schedules);
+      // console.log(this.schedules);
     });
   }
 
   onClickGenerateAppointments() {
-    console.log("generowanie wizyt...")
+    const generateAppointmentsRequest = new GenerateAppointmentsRequest(this.currentUser.username, this.generateStart, this.generateEnd);
+    this.appointmentService.generateAppointments(generateAppointmentsRequest).subscribe();
   }
 }
